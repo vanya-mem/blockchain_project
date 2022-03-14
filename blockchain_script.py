19e16927daaf4cdecd2360e02a559d0a77529fe2
vanya-mem <71513027+vanya-mem@users.noreply.github.com>
2022-03-14 21:33:27 +0300

    add some features

--- blockchain_script.py
+++ blockchain_script.py
@@ -6,19 +6,23 @@ import hashlib
 BLOCK_CHAIN_DIRECTION = os.path.abspath(os.curdir) + r'\blockchain'
 
 
-def get_hash(filename):
-    file = open(BLOCK_CHAIN_DIRECTION + r'\{}'.format(filename), 'rb').read()
+def get_hash(student_folder, filename):
+    file = open(BLOCK_CHAIN_DIRECTION + r'\{}'.format(student_folder) + r'\{}'.format(filename), 'rb').read()
 
     return hashlib.sha256(file).hexdigest()
 
 
-def get_files():
-    files = os.listdir(BLOCK_CHAIN_DIRECTION)
+def get_files(student_folder):
+    if os.path.exists(BLOCK_CHAIN_DIRECTION + r'\{}'.format(student_folder)):
+        files = os.listdir(BLOCK_CHAIN_DIRECTION + r'\{}'.format(student_folder))
+    else:
+        os.mkdir(BLOCK_CHAIN_DIRECTION + r'\{}'.format(student_folder))
+        files = os.listdir(BLOCK_CHAIN_DIRECTION + r'\{}'.format(student_folder))
     return files
 
 
-def sort_files():
-    files = get_files()
+def sort_files(student_folder):
+    files = get_files(student_folder)
     files = [int(i) for i in files]
 
     last_file = files[-1]
@@ -27,17 +31,17 @@ def sort_files():
     return last_file, filename
 
 
-def block_check():
-    files = get_files()
+def block_check(student_folder):
+    files = get_files(student_folder)
     files = [int(i) for i in files]
 
     result = []
 
     for elem in files[1:]:
-        hsh = json.load(open(BLOCK_CHAIN_DIRECTION + r'\{}'.format(elem)))['Хэш']  # Хэш n-го файла (без учета 1)
+        hsh = json.load(open(BLOCK_CHAIN_DIRECTION + r'\{}'.format(student_folder) + r'\{}'.format(elem)))['Хэш']  # Хэш n-го файла (без учета 1)
 
         last_file = str(elem - 1)
-        actual_hash = get_hash(last_file)  # Хэш файла n - 1
+        actual_hash = get_hash(student_folder, last_file)  # Хэш файла n - 1
 
         if actual_hash == hsh:
             res = 'Всё в порядке'
@@ -51,7 +55,8 @@ def block_check():
 
 def write_block(f_and_s_name, lesson, mark, date, prev_hash=''):
 
-    files = get_files()
+    f_and_s_name = f_and_s_name.upper()
+    files = get_files(student_folder=f_and_s_name)
 
     if len(files) == 0:
         data = {
@@ -62,13 +67,13 @@ def write_block(f_and_s_name, lesson, mark, date, prev_hash=''):
             'Хэш': prev_hash
         }
 
-        with open(BLOCK_CHAIN_DIRECTION + r'\1', 'w', encoding='cp1251') as file:
+        with open(BLOCK_CHAIN_DIRECTION + r'\{}'.format(f_and_s_name) + r'\1', 'w', encoding='cp1251') as file:
             json.dump(data, file, ensure_ascii=False, indent=4)
     else:
 
-        last_file, filename = sort_files()
+        last_file, filename = sort_files(student_folder=f_and_s_name)
 
-        prev_hash = get_hash(str(last_file))
+        prev_hash = get_hash(f_and_s_name, str(last_file))
         data = {
             'Фамилия, имя': f_and_s_name,
             'Урок': lesson,
@@ -77,13 +82,14 @@ def write_block(f_and_s_name, lesson, mark, date, prev_hash=''):
             'Хэш': prev_hash
         }
 
-        with open(BLOCK_CHAIN_DIRECTION + r'\{}'.format(filename), 'w', encoding='cp1251') as file:
+        with open(BLOCK_CHAIN_DIRECTION + r'\{}'.format(f_and_s_name) + r'\{}'.format(filename), 'w', encoding='cp1251') as file:
             json.dump(data, file, ensure_ascii=False, indent=4)
 
 
 def main():
-    write_block('Example', 'Example', 'Example', 'Example')  # ///
-    # Тип данных: str(Фамилия, имя), srt(Название урока), str или int - оценка, str(Дата). Формат: дд.мм.гг
+    write_block('ExaPLe1', 'Example', 'Example', 'Example')  # ///
+    print(block_check(student_folder='ExaPLe1')) #student_folder - Имя и фамилия ученика. Указывать так же, как в перемен. f_and_s_name
+    # Тип данных: str(Фамилия, имя), srt(Нахвание урока), str или int - оценка, str(Дата). Формат: дд.мм.гг
 
 
 if __name__ == '__main__':
